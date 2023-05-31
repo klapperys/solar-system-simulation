38c0da33971eb2122eb94e5700c8c37744c7e1a0
Emile Badenhorst <MyvarHD@gmail.com>
2023-05-31 13:28:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-import dataclasses
-import sys
+from time import sleep
 import pygame
-from pygame.locals import *
 
-#properties of sun
-mass = 10000
-
-#GUI
+# GUI Setup
 pygame.init()
 pygame.display.init()
-window = pygame.display.set_mode((1000, 1000))
-red = (255,0,0)
-circleX = 500
-circleY = 400
-radius = 10
-sun = pygame.draw.circle(window, red, (circleX, circleY), radius),(mass)
-pygame.display.update()
-while True:
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# create window
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("N-Body Simulation")
+
+sw2 = SCREEN_WIDTH / 2
+sh2 = SCREEN_HEIGHT / 2
+
+# mass: 0, name: 1, pos: 2, color: 3, raduis: 4
+sun = [10000, "sun", [sw2, sh2], (255, 0, 0), 10]
+earth = [100, "earth", [sw2 + 100, sh2], (0, 255, 0), 5]
+
+planets = [sun, earth]
+
+
+def update_simulation():
     pass
 
 
+def draw_body(body):
+    pygame.draw.circle(window, body[3], (body[2][0], body[2][1]), body[4])
+
+
+def render_frame():
+    for body in planets:
+        draw_body(body)
+
+
+should_be_running = True
+while should_be_running:
+    # handel events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            should_be_running = False
+
+    # clear -> draw -> swap buffer
+    window.fill((0, 0, 0))
+    update_simulation()
+    render_frame()
+    pygame.display.flip()
+    sleep(1 / 60)
